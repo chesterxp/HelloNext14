02435103d14fb546627968351cab917b9ed2e0cf
Łukasz Zawiślak <lukasz.zawislak@agora.pl>
2024-10-16 09:37:15 +0200

    First Commit

--- src/app/page.js
+++ src/app/page.js
@@ -18,6 +18,9 @@ export default function Home() {
             Get started by editing <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Hello Next</li>
+          <li>First Commit</li>
+          
         </ol>
 
         <div className={styles.ctas}>
